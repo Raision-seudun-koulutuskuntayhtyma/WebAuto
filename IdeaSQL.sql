9173250089d06125c09c67fb40d8fab87280f0e0
Mika Vainio <koti.mika.vainio@gmail.com>
2025-09-29 15:58:04 +0300

    Query to get data for vehicles page

--- IdeaSQL.sql
+++ IdeaSQL.sql
@@ -1,15 +1,32 @@
+-- Select all vehicles and set status to vapaana (available)
 SELECT auto.rekisterinumero,
 	auto.merkki,
 	auto.malli,
 	auto.henkilomaara,
-	auto.automaatti
+	auto.automaatti,
+	text 'vapaana' AS status
 FROM public.auto
 WHERE auto.kaytettavissa = TRUE
+
+-- Use set theory's except operation to remove vehicles in use but set status also to vapaana (available)
+-- to get correct result set
 EXCEPT
 SELECT auto.rekisterinumero,
 	auto.merkki,
 	auto.malli,
 	auto.henkilomaara,
-	auto.automaatti
+	auto.automaatti,
+	text 'vapaana' AS status
+FROM public.auto INNER JOIN public.lainaus ON auto.rekisterinumero = lainaus.rekisterinumero
+WHERE auto.kaytettavissa = TRUE AND lainaus.palautusaika IS NULL
+
+-- Add vehicles in use using set theory's union operation and set status to ajossa (in use)
+UNION
+SELECT auto.rekisterinumero,
+	auto.merkki,
+	auto.malli,
+	auto.henkilomaara,
+	auto.automaatti,
+	text 'ajossa' AS status
 FROM public.auto INNER JOIN public.lainaus ON auto.rekisterinumero = lainaus.rekisterinumero
 WHERE auto.kaytettavissa = TRUE AND lainaus.palautusaika IS NULL;
